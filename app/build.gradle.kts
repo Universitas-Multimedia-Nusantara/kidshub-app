a7a150a0a7577c832f295f7efcae61f939b5ca56
M. Naufal Syarif <87231791+palsarip@users.noreply.github.com>
2023-12-19 16:14:29 +0700

    feat: adding bottom sheets and updadting activity-compose dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,7 +61,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.1")
+    implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
@@ -130,4 +130,7 @@ dependencies {
 
     //Android Youtube Player
     implementation("com.pierfrancescosoffritti.androidyoutubeplayer:chromecast-sender:0.28")
+
+    //Bottom Sheets
+    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
 }
