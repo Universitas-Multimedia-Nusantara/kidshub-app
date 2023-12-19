9c5f1ef65cf2facd6fc038fdff40877994aae100
M. Naufal Syarif <87231791+palsarip@users.noreply.github.com>
2023-12-19 21:10:20 +0700

    feat: adding videos bottom navigation

--- app/src/main/java/id/ac/umn/kidshub/navigation/BottomNavigationItem.kt
+++ app/src/main/java/id/ac/umn/kidshub/navigation/BottomNavigationItem.kt
@@ -4,6 +4,11 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material.icons.rounded.Book
+import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material.icons.rounded.OndemandVideo
+import androidx.compose.material.icons.rounded.Person
+import androidx.compose.material.icons.rounded.VideoLibrary
 import androidx.compose.ui.graphics.vector.ImageVector
 
 data class BottomNavigationItem (
@@ -15,17 +20,22 @@ data class BottomNavigationItem (
 val listOfBottomNavigationItems: List<BottomNavigationItem> = listOf(
     BottomNavigationItem(
         title = "Home",
-        selectedIcon = Icons.Default.Home,
-        unselectedIcon = Icons.Default.Home
+        selectedIcon = Icons.Rounded.Home,
+        unselectedIcon = Icons.Rounded.Home,
+    ),
+    BottomNavigationItem(
+        title = "Videos",
+        selectedIcon = Icons.Rounded.OndemandVideo,
+        unselectedIcon = Icons.Rounded.OndemandVideo,
     ),
     BottomNavigationItem(
         title = "Books",
-        selectedIcon = Icons.Default.ShoppingCart,
-        unselectedIcon = Icons.Default.ShoppingCart
+        selectedIcon = Icons.Rounded.Book,
+        unselectedIcon = Icons.Rounded.Book,
     ),
     BottomNavigationItem(
         title = "Profile",
-        selectedIcon = Icons.Default.Person,
-        unselectedIcon = Icons.Default.Person
+        selectedIcon = Icons.Rounded.Person,
+        unselectedIcon = Icons.Rounded.Person,
     )
 )
