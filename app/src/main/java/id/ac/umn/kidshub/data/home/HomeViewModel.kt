6686acd3bdd93030e7c8760c63a009aedd00f137
M. Naufal Syarif <87231791+palsarip@users.noreply.github.com>
2023-12-19 16:16:32 +0700

    feat: adding time, videos, and books function call from firestore

--- app/src/main/java/id/ac/umn/kidshub/data/home/HomeViewModel.kt
+++ app/src/main/java/id/ac/umn/kidshub/data/home/HomeViewModel.kt
@@ -1,19 +1,23 @@
 package id.ac.umn.kidshub.data.home
 
 import android.util.Log
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import id.ac.umn.kidshub.data.home.books.BooksData
+import id.ac.umn.kidshub.data.home.books.BooksDataProvider
 import id.ac.umn.kidshub.data.home.users.UsersData
 import id.ac.umn.kidshub.data.home.users.UsersDataProvider
+import id.ac.umn.kidshub.data.home.videos.VideosData
+import id.ac.umn.kidshub.data.home.videos.VideosDataProvider
 import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
+import java.util.Calendar
 
 class HomeViewModel: ViewModel(), CoroutineScope {
 
@@ -22,6 +26,10 @@ class HomeViewModel: ViewModel(), CoroutineScope {
     val userData = UsersDataProvider.usersData
 
     val isUserLoggedIn: MutableLiveData<Boolean?> = MutableLiveData()
+
+    private val currentTime = System.currentTimeMillis()
+
+    val greeting = getGreetingByTime(currentTime)
 
     fun checkForActiveSession() {
         if(FirebaseAuth.getInstance().currentUser != null) {
@@ -35,8 +43,17 @@ class HomeViewModel: ViewModel(), CoroutineScope {
         }
     }
 
-    fun getCurrentTime (): String {
-        return System.currentTimeMillis().toString()
+    private fun getGreetingByTime(currentTime: Long): String {
+        val calendar = Calendar.getInstance()
+        calendar.timeInMillis = currentTime
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)
+
+        return when {
+            hour < 12 -> "Good Morning !"
+            hour < 16 -> "Good Afternoon !"
+            hour < 21 -> "Good Evening !"
+            else -> "Good Night !"
+        }
     }
 
     suspend fun getUserData(): UsersData {
@@ -78,5 +95,101 @@ class HomeViewModel: ViewModel(), CoroutineScope {
         return deferred.await()
     }
 
+    suspend fun getVideosData(): List<VideosData>  {
+        val deferred = CompletableDeferred<List<VideosData>>()
+
+        coroutineScope {
+            launch(Dispatchers.IO) {
+                try {
+                    val db = FirebaseFirestore.getInstance()
+
+                    db.collection("videos")
+                        .get()
+                        .addOnSuccessListener { snapshot ->
+                            for (document in snapshot) {
+                                val videosData = VideosData(
+                                    document.data["id"].toString(),
+                                    document.data["title"].toString(),
+                                    document.data["thumbnail"].toString(),
+                                    document.data["url"].toString(),
+                                    document.data["description"].toString(),
+                                    document.data["uploader"].toString(),
+                                )
+
+                                VideosDataProvider.videosDataList.add(videosData)
+
+                                Log.d(TAG, "${document.id} => ${document.data}")
+                            }
+                        }
+                        .await()
+                    deferred.complete(VideosDataProvider.videosDataList)
+                } catch (e: Exception) {
+                    Log.d(TAG, "Exception: $e")
+                    deferred.completeExceptionally(e)
+                }
+            }
+        }
+        return deferred.await()
+    }
+
+    suspend fun getBooksData(): List<BooksData>  {
+        val deferred = CompletableDeferred<List<BooksData>>()
+
+        coroutineScope {
+            launch(Dispatchers.IO) {
+                try {
+                    val db = FirebaseFirestore.getInstance()
+
+                    db.collection("books")
+                        .get()
+                        .addOnSuccessListener { snapshot ->
+                            for (document in snapshot) {
+                                val booksData = BooksData(
+                                    document.data["id"].toString(),
+                                    document.data["cover"].toString(),
+                                    document.data["title"].toString(),
+                                    document.data["author"].toString(),
+                                    document.data["description"].toString(),
+                                    document.data["publisher"].toString(),
+                                )
+
+                                BooksDataProvider.booksDataList.add(booksData)
+
+                                Log.d(TAG, "${document.id} => ${document.data}")
+                            }
+                        }
+                        .await()
+                    deferred.complete(BooksDataProvider.booksDataList)
+                } catch (e: Exception) {
+                    Log.d(TAG, "Exception: $e")
+                    deferred.completeExceptionally(e)
+                }
+            }
+        }
+        return deferred.await()
+    }
+
+    fun updateUserExp() {
+        val userId = FirebaseAuth.getInstance().currentUser?.uid
+        val db = FirebaseFirestore.getInstance()
+
+        val document = db.collection("users")
+            .document(userId!!)
+            .update("exp", expIncrement(userData.exp))
+            .addOnSuccessListener {
+
+                userData.exp = expIncrement(userData.exp)
+
+                Log.d(TAG, "Exp successfully updated!")
+            }
+            .addOnFailureListener { e ->
+                Log.d(TAG, "Error updating document", e)
+            }
+    }
+
+    private fun expIncrement (exp: Int): Int {
+       val  randomInt = (25..100).random()
+        return exp + randomInt
+    }
     override val coroutineContext = Dispatchers.Main
 }
